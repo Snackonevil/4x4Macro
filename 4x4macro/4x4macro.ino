2efa2b51e1e9512cffe841edafa144b91bb1ee3d
Kevin <lacsonky@gmail.com>
2025-03-21 05:30:31 -0400

    LED update

--- 4x4macro/4x4macro.ino
+++ 4x4macro/4x4macro.ino
@@ -23,7 +23,13 @@ byte rowPins[ROWS] = { R1, R2, R3, R4 };
 byte colPins[COLS] = { C1, C2, C3, C4 };
 Keypad kpd = Keypad(makeKeymap(keys), colPins, rowPins, COLS, ROWS);
 
+int lastDebounceTime = 4;
+const int debounceTime = 50;
 
+int LED_MATRIX[16] = { B10000111, B01000111, B00100111, B00010111,
+                       B10001011, B01001011, B00101011, B00011011,
+                       B10001101, B01001101, B00101101, B00011101,
+                       B10001110, B01001110, B00101110, B00011110 };
 //Shift Register
 
 // ST_CP pin 12
@@ -37,6 +43,19 @@ const int dataPin = 14;
 const int numStates = 4;
 int currentState = 0;
 
+void clearRegister() {
+  digitalWrite(latchPin, LOW);
+  shiftOut(latchPin, clockPin, LSBFIRST, B00001111);
+  digitalWrite(latchPin, HIGH);
+}
+
+void flashLED(int time, uint8_t LED) {
+  digitalWrite(latchPin, LOW);
+  delay(time);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED);
+  digitalWrite(latchPin, HIGH);
+}
+
 void setup() {
 
   // Serial.begin(9600);
@@ -47,13 +66,13 @@ void setup() {
   //   digitalWrite(States[i], LOW);
   // }
   // Shift Register set up
-  // pinMode(latchPin, OUTPUT);
-  // pinMode(clockPin, OUTPUT);
-  // pinMode(dataPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  clearRegister();
   Keyboard.begin();
 
-
-  Serial.print("Ready");
+  // Serial.print("Ready");
 }
 
 
@@ -64,56 +83,72 @@ void ChangeState() {
     currentState = 0;
   }
   //Serial.print("State Changed. Current State: "); Serial.println(currentState);
-  delay(100);
+  flashLED(100, LED_MATRIX[3]);
+  // delay(100);
   return;
 }
 //NUMPAD
 void Layout1(char button) {
   switch (button) {
     case '1':
+      flashLED(100, LED_MATRIX[0]);
       Keyboard.print('7');
       Keyboard.releaseAll();
       break;
     case '2':
       Keyboard.print('8');
+      flashLED(100, LED_MATRIX[1]);
       break;
     case '3':
+      flashLED(100, LED_MATRIX[2]);
       Keyboard.print('9');
       break;
     case '5':
+      flashLED(100, LED_MATRIX[4]);
       Keyboard.print('4');
       break;
     case '6':
+      flashLED(100, LED_MATRIX[5]);
       Keyboard.print('5');
       break;
     case '7':
+      flashLED(100, LED_MATRIX[6]);
       Keyboard.print('6');
       break;
     case '8':
+      flashLED(100, LED_MATRIX[7]);
       Keyboard.print('+');
       break;
     case '9':
+      flashLED(100, LED_MATRIX[8]);
       Keyboard.print('1');
       break;
     case '0':
+      flashLED(100, LED_MATRIX[9]);
       Keyboard.print('2');
       break;
     case 'a':
+      flashLED(100, LED_MATRIX[10]);
       Keyboard.print('3');
       break;
     case 'b':
+      flashLED(100, LED_MATRIX[11]);
       Keyboard.print('-');
       break;
     case 'c':
+      flashLED(100, LED_MATRIX[12]);
       Keyboard.print('0');
       break;
     case 'd':
+      flashLED(100, LED_MATRIX[13]);
       Keyboard.print('0');
       break;
     case 'e':
+      flashLED(100, LED_MATRIX[14]);
       Keyboard.print('*');
       break;
     case 'f':
+      flashLED(100, LED_MATRIX[15]);
       Keyboard.press(KEY_ENTER);
       Keyboard.releaseAll();
       break;
@@ -221,19 +256,13 @@ void Layout4(char button) {
   };
 }
 
+
 void loop() {
-  // digitalWrite(latchPin, LOW);
-  // // delay(1000);
-  // shiftOut(dataPin, clockPin, LSBFIRST, B10000111);
-  // digitalWrite(latchPin, HIGH);
-  // // Serial.println("one");
-  // // delay(1000);
-  // // digitalWrite(latchPin, LOW);
-  // // delay(1000);
-  // // shiftOut(dataPin, clockPin, LSBFIRST, B01001011);
-  // // digitalWrite(latchPin, HIGH);
-  // // Serial.println("two");
-  // // delay(1000);
+  //   int i;
+  //   for (i = 0; i < 16; i++)
+  // {
+  //   flashLED(100, LED_MATRIX[i]);
+  //  }
   //check the key matrix first
   char key = kpd.getKey();
   if (key) {
@@ -263,5 +292,5 @@ void loop() {
             break;
         }
     }
-  }0
+  }
 }
