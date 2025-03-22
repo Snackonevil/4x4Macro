d6bf0a0738e004c235774d8ff7acd8988c05d0b3
Kevin <lacsonky@gmail.com>
2025-03-22 00:20:29 -0400

    LED animations and macros

--- 4x4macro/4x4macro.ino
+++ 4x4macro/4x4macro.ino
@@ -30,6 +30,10 @@ int LED_MATRIX[16] = { B10000111, B01000111, B00100111, B00010111,
                        B10001011, B01001011, B00101011, B00011011,
                        B10001101, B01001101, B00101101, B00011101,
                        B10001110, B01001110, B00101110, B00011110 };
+
+int LED_ROWS[4] = { B11110111, B11111011, B11111101, B11111110 };
+int LED_COLS[4] = { B10000000, B01000000, B00100000, B00010000 };
+
 //Shift Register
 
 // ST_CP pin 12
@@ -51,11 +55,25 @@ void clearRegister() {
 
 void flashLED(int time, uint8_t LED) {
   digitalWrite(latchPin, LOW);
-  delay(time);
+  delay(100);
   shiftOut(dataPin, clockPin, LSBFIRST, LED);
   digitalWrite(latchPin, HIGH);
 }
 
+void startUpAnimation() {
+  for (int i = 0; i < 4; i++) {
+    flashLED(100, LED_ROWS[i]);
+  }
+  for (int j = 0; j < 4; j++) {
+    flashLED(100, LED_COLS[j]);
+  }
+  for (int k = 2; k >= 0; k--) {
+    flashLED(100, LED_COLS[k]);
+  }
+  for (int l = 3; l > 0; l--) {
+    flashLED(100, LED_ROWS[l]);
+  }
+}
 void setup() {
 
   // Serial.begin(9600);
@@ -69,6 +87,7 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  startUpAnimation();
   clearRegister();
   Keyboard.begin();
 
@@ -87,6 +106,7 @@ void ChangeState() {
   // delay(100);
   return;
 }
+
 //NUMPAD
 void Layout1(char button) {
   switch (button) {
@@ -177,8 +197,6 @@ void Layout2(char button) {
     case '6':  //
       break;
     case '7':  //
-      Keyboard.print("lacsonky@gmail.com");
-      Keyboard.releaseAll();
       break;
     case '8':  //
       break;
@@ -225,44 +243,237 @@ void Layout3(char button) {
 }
 
 void Layout4(char button) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  int l = 0;
+  int m = 0;
   switch (button) {
     case '1':
-      Keyboard.print('1');
+      flashLED(100, LED_MATRIX[0]);
+      Keyboard.press(KEY_TAB);
+      Keyboard.releaseAll();
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (i = 0; i < 14; i++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (j = 0; j < 21; j++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (k = 0; k < 7; k++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (l = 0; l < 4; l++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (m = 0; m < 10; m++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_ENTER);
+      Keyboard.releaseAll();
       break;
+
     case '2':
-      Keyboard.print('2');
+      flashLED(100, LED_MATRIX[1]);
+      for (i = 0; i < 24; i++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (j = 0; j < 20; j++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (k = 0; k < 3; k++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (l = 0; l < 10; l++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_ENTER);
+      Keyboard.releaseAll();
       break;
+
     case '3':
-      Keyboard.print('3');
-      break;
-    case '4':
-      Keyboard.print('4');
+      flashLED(100, LED_MATRIX[2]);
+      for (i = 0; i < 2; i++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (j = 0; j < 42; j++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (k = 0; k < 3; k++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (l = 0; l < 10; l++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_ENTER);
+      Keyboard.releaseAll();
       break;
     case '5':
-      Keyboard.print('5');
+      flashLED(100, LED_MATRIX[4]);
+      for (i = 0; i < 6; i++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (k = 0; k < 41; k++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (l = 0; l < 10; l++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_ENTER);
+      Keyboard.releaseAll();
       break;
+
     case '6':
-      Keyboard.print('6');
+      flashLED(100, LED_MATRIX[5]);
+      for (i = 0; i < 21; i++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (k = 0; k < 26; k++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (l = 0; l < 10; l++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_ENTER);
+      Keyboard.releaseAll();
       break;
     case '7':
+      flashLED(100, LED_MATRIX[6]);
+      for (i = 0; i < 15; i++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
 
+      for (j = 0; j < 29; j++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (k = 0; k < 3; k++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_SPACE);
+      Keyboard.releaseAll();
+
+      for (l = 0; l < 10; l++) {
+        Keyboard.press(KEY_TAB);
+        Keyboard.releaseAll();
+      }
+      Keyboard.press(KEY_ENTER);
+      Keyboard.releaseAll();
       break;
     case '8':
-
+      flashLED(100, LED_MATRIX[7]);
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press('r');
+      Keyboard.releaseAll();
       break;
     case '9':
-
+      flashLED(100, LED_MATRIX[8]);
+      Keyboard.print('1');
+      break;
+    case '0':
+      flashLED(100, LED_MATRIX[9]);
+      Keyboard.print('2');
+      break;
+    case 'a':
+      flashLED(100, LED_MATRIX[10]);
+      Keyboard.print('3');
+      break;
+    case 'b':
+      flashLED(100, LED_MATRIX[11]);
+      Keyboard.print('-');
+      break;
+    case 'c':
+      flashLED(100, LED_MATRIX[12]);
+      Keyboard.print('0');
+      break;
+    case 'd':
+      flashLED(100, LED_MATRIX[13]);
+      Keyboard.print('0');
+      break;
+    case 'e':
+      flashLED(100, LED_MATRIX[14]);
+      Keyboard.print('*');
+      break;
+    case 'f':
+      flashLED(100, LED_MATRIX[15]);
+      Keyboard.press(KEY_ENTER);
+      Keyboard.releaseAll();
       break;
   };
 }
 
 
 void loop() {
-  //   int i;
-  //   for (i = 0; i < 16; i++)
-  // {
-  //   flashLED(100, LED_MATRIX[i]);
-  //  }
   //check the key matrix first
   char key = kpd.getKey();
   if (key) {
